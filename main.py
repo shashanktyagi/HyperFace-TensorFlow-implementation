91391eefe0aed28388f48f816a707bae93132797
Shashank Tyagi <shashank@shashanks-mbp.dynamic.ucsd.edu>
2017-05-04 23:15:59 -0700

    losses added

---
+++ main.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import os
+from model import *
+
+if not os.path.exists('./logs'):
+	os.makedirs('./logs')
+
+map(os.unlink, (os.path.join( './logs',f) for f in os.listdir('./logs')) )
+
+
+with tf.Session() as sess:
+		print 'Building Graph...'
+		net = HyperFace(sess)
+		print 'Graph Built!'
+		sess.run(tf.global_variables_initializer())
+		net.print_variables()
+		# net.train()
+
