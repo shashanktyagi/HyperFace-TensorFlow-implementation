5b1e9739b199c0185ef64b0ede57bc1cc92dca2d
Shashank Tyagi <shashank@Shashanks-MacBook-Pro.local>
2017-05-09 15:13:46 -0700

    add training method

--- with SPN/main.py
+++ with SPN/main.py
@@ -4,16 +4,17 @@ from model import *
 
 weights_path = '/Users/shashank/Tensorflow/SPN/weights/'
 imgs_path = '/Users/shashank/Tensorflow/CSE252C-Hyperface/git/truth_data.npy'
-tf_record_file_path = 'aflw_train.tfrecords'
+tf_record_file_path = '../aflw_train.tfrecords'
 if not os.path.exists('./logs'):
 	os.makedirs('./logs')
 
 map(os.unlink, (os.path.join( './logs',f) for f in os.listdir('./logs')) )
 
 
+
 with tf.Session() as sess:
 		print 'Building Graph...'
-		model = Network(sess,tf_record_file_path=tf_record_file_path)
+		model = Network(sess,tf_record_file_path)
 		print 'Graph Built!'
 		sess.run(tf.global_variables_initializer())
 		model.train()
