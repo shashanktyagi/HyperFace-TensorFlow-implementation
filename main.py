564e3a8a191160ad1ce833a42cb6a638c931b739
shashank <345ishaan@gmail.com>
2017-05-25 23:16:42 -0700

    bug

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ if not os.path.exists('./logs'):
 
 map(os.unlink, (os.path.join( './logs',f) for f in os.listdir('./logs')) )
 
-net = HyperFace(tf_record_file_path='../../aflw_train_small_new.tfrecords')
+net = HyperFace(False,tf_record_file_path='../../aflw_train_small_new.tfrecords',model_save_path='../../checkpoint',best_model_save_path='../../best_checkpoint')
 
 with tf.Session() as sess:
 		print 'Building Graph...'
-		net.build_network(sess,False,model_save_path='../../checkpoint',best_model_save_path='../../best_checkpoint')
+		net.build_network(sess)
 		print 'Graph Built!'
 		# net.print_variables()
 		# net.load_weights(weights_path)
